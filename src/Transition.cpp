eee5acb0f3a65b65311d3bc81818dd4bea177ada
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-04-24 14:17:38 -0500

    Major refactoring and simplification.

--- src/Transition.cpp
+++ src/Transition.cpp
@@ -18,82 +18,47 @@
  * Contact:     isp-dev@cs.utah.edu
  */
 
-#ifdef CONFIG_DEBUG_SCHED
-#define DR(code) if(Scheduler::_debug) { code }
-#define DS(code) { code }
-#else
-#define DR(code)
-#define DS(code)
-#endif
-
 #include "Transition.hpp"
 
 #include <signal.h>
-
+/*
 Transition::Transition() {
-    DS( t = NULL; );
 }
 
-Transition::Transition(const Transition &t_, bool ref_t) {
+Transition::Transition(const Transition &t_) {
     *this = t_;
-    if(ref_t) t->ref_inc();
 }
 
-Transition::Transition(Envelope *e, bool ref_e) {
-    t = new Transition_internal(e);
-    if(ref_e) t->GetEnvelope()->ref_inc();
-}
+Transition::Transition(std::unique_ptr<Envelope> e) : envelope(std::move(e)) {
+    
+}*/
 
-Transition &Transition::operator=(const Transition &t_) {
-    t = t_.t;
-    return *this;
-}
-
-void Transition::ref() {
-    t->ref_inc();
-}
-
-void Transition::unref() {
-    if(!t->isshared()) {
-        delete t;
-        DS( t = NULL; )
-    } else t->ref_dec();
-}
-
-void Transition::copy() {
-    if(t->isshared()) {
-        t->ref_dec();
-        t = new Transition_internal(*t);
-    }
-}
-
-Envelope *Transition::GetEnvelope() const {
-    return t->GetEnvelope();
-}
-
-bool Transition::AddIntraCB(CB &c) {
-    if(t->AddIntraCB_check(c)) {
-        copy();
-        t->AddIntraCB_mutate(c);
+bool Transition::AddIntraCB(std::unique_ptr<CB> c) {
+    if(IsNew(*c)) {
+        intra_cb.push_back(std::move(c));
         return true;
     }
     return false;
 }
 
-void Transition::AddInterCB(CB &c) {
-    if(t->AddInterCB_check(c)) {
-        copy();
-        t->AddInterCB_mutate(c);
+bool Transition::AddInterCB(std::unique_ptr<CB> c) {
+    if (c->_pid == -1 && c->_index == -1) {
+        inter_cb.push_back (std::move(c));
+        return false;
     }
+    if(IsNew(*c)) {
+        inter_cb.push_back(std::move(c));
+        return true;
+    }
+    return false;
 }
 
-void Transition::set_curr_matching(const CB &c) {
-    copy();
-    t->curr_matching = c;
+void Transition::set_curr_matching(std::unique_ptr<CB> c) {
+    t->curr_matching = std::move(c);
 }
 
 CB &Transition::get_curr_matching() const {
-    return t->curr_matching;
+    return *t->curr_matching;
 }
 
 
@@ -102,115 +67,14 @@ std::vector<int> &Transition::get_ancestors() const {
 }
 
 std::vector<int> &Transition::mod_ancestors() {
-    copy();
     return t->ancestors;
 }
-
-
-#ifdef FIB
-std::vector <CB> &Transition::get_inter_cb() {
-    return t->_inter_cb;
-}
-
-std::vector <CB> &Transition::mod_inter_cb() {
-    copy();
-    return t->_inter_cb;
-}
-
-std::vector <CB> &Transition::get_intra_cb() {
-    return t->_intra_cb;
-}
-
-std::vector <CB> &Transition::get_cond_intra_cb() {
-    return t->_cond_intra_cb;
-}
-    
-void Transition::AddCondIntraCB(CB &c) {
-    if(t->AddCondIntraCB_check(c)) {
-        copy();
-        t->AddCondIntraCB_mutate(c);
-    } 
-}
-#endif
-
-Transition_internal::Transition_internal (Envelope * env) {
-    _env = env;
-    ref = 1;
-    curr_matching = CB(-1,-1);
-}
-
-Transition_internal::Transition_internal (Transition_internal &t) {
-    (*this) = t;
-}
-
-Transition_internal::~Transition_internal() {
-    if(!_env->isshared()) {
-        delete _env;
-        DS( _env = NULL; )
-    } else _env->ref_dec();
-}
-
-Transition_internal &Transition_internal::operator= (Transition_internal &t) {
-    ref = 1;
-    curr_matching = t.curr_matching;
-    _env = t.GetEnvelope();
-    _env->ref_inc();
-    ancestors = t.ancestors;
-    _intra_cb = t._intra_cb;
-    _inter_cb = t._inter_cb;
-#ifdef FIB
-    _cond_intra_cb = t._cond_intra_cb;
-#endif
-    return *this;
-}
-
-bool Transition_internal::AddIntraCB_check(CB &c) {
-    return !Contains(c);
-}
-
-void Transition_internal::AddIntraCB_mutate(CB &c) {
-    _intra_cb.push_back(c);
-}
-
-bool Transition_internal::AddInterCB_check(CB &c) {
-    std::vector <CB>::iterator iter;
-    std::vector <CB>::iterator iter_end = _intra_cb.end();
-
-    if (c._pid == -1 && c._index == -1) {
-        _inter_cb.push_back (c);
-        return false;
-    }
-    return !Contains(c);
-}
-void Transition_internal::AddInterCB_mutate(CB &c) {
-    _inter_cb.push_back(c);
-}
-
-#ifdef FIB
-bool Transition_internal::AddCondIntraCB_check(CB &c)
-{
-    return !vec_contains(_cond_intra_cb) && !vec_contains(_inter_cb);
-}
-
-void Transition_internal::AddCondIntraCB_mutate(CB &c) {
-    _cond_intra_cb.push_back(c);
-}
-#endif
+/*
+Transition_internal::Transition_internal (std::unique_ptr<Envelope> env) : curr_matching(new CB(-1,-1)) {
+    envelope = std::move(env);
+}*/
 
 bool Transition_internal::is_curr_matching_set() {
-    return curr_matching != CB(-1,-1);
+    return *curr_matching != CB(-1,-1);
 }
 
-void Transition_internal::ref_inc() {
-    ref++;
-}
-
-void Transition_internal::ref_dec() {
-    DS( assert(ref>1); )
-    ref--;
-}
-
-bool Transition_internal::isshared() {
-    DS( assert(ref>0); )
-    return ref != 1;
-}
--- src/Transition.hpp
+++ src/Transition.hpp
@@ -22,125 +22,60 @@
 #define _TRANSITION_HPP
 
 #include "Envelope.hpp"
+#include "CB.hpp"
 #include <vector>
 #include <cassert>
-
-/*
- * This implements a single transition
- */
-class CB {
-public:
-    int _pid;
-    int _index;
-    CB () { }
-
-    CB (int i, int ind) : _pid(i), _index(ind) {
-    }
-    CB(const CB& c) : _pid(c._pid), _index(c._index){}
-    bool operator== (const CB &c) {
-        return _pid == c._pid && _index == c._index;
-    }
-    bool operator!= (const CB &c) {
-        return !(*this == c);
-    }
-    friend bool operator< (const CB &a, const CB &b) {
-        return a._pid < b._pid || (a._pid == b._pid && a._index < b._index);
-    }
-    CB& operator= (const CB &c) {
-        this->_pid = c._pid;
-        this->_index = c._index;
-        return *this;
-    }
-};
+#include <memory>
 
 class Transition_internal {
 public:
-    Transition_internal();
-    ~Transition_internal();
-    Transition_internal(Envelope * env);
-    /*
-      * Copy ctor and assignment operator
-     */
-    Transition_internal(Transition_internal &t);
-    Transition_internal& operator=(Transition_internal &);
-
-    inline Envelope *GetEnvelope () {return _env;}
-    bool AddIntraCB_check(CB &c);
-    void AddIntraCB_mutate(CB &c);
-    bool AddInterCB_check(CB &c);
-    void AddInterCB_mutate(CB &c);
     bool is_curr_matching_set();
-    
-#ifdef FIB
-    bool AddCondIntraCB_check(CB &c);
-    void AddCondIntraCB_mutate(CB &c);
-#endif
-    void ref_inc();
-    void ref_dec();
-    bool isshared();
-
-    std::vector <CB> _inter_cb;
-    std::vector <CB> _intra_cb;
-#ifdef FIB
-    std::vector <CB> _cond_intra_cb;
-#endif
 
     std::vector<int> ancestors;
-    CB curr_matching;
-    unsigned int ref;
+    std::unique_ptr<CB> curr_matching;
+};
+
+class Transition {
+public:
+    /*
+    Transition();
+    Transition(const Transition &t_);
+    Transition(std::unique_ptr<Envelope> e);
+    */
+    //Transition &operator=(const Transition &t_);
+
+    inline Envelope& GetEnvelope () const {return *envelope;}
+    bool AddIntraCB(std::unique_ptr<CB> c);
+    bool AddInterCB(std::unique_ptr<CB> c);
+    void moveCurrMatching();
+    void set_curr_matching(std::unique_ptr<CB> c);
+    CB &get_curr_matching() const;
+    std::vector<int> &get_ancestors() const;
+    std::vector<int> &mod_ancestors();
+
+    std::unique_ptr<Transition_internal> t;
 private:
-    Envelope     *_env;
+    std::unique_ptr<Envelope> envelope;
+    std::vector <std::unique_ptr<CB> > inter_cb;
+    std::vector <std::unique_ptr<CB> > intra_cb;
     /**
      * Tests if an element is in the vector.
      */
-    inline static bool vec_contains(std::vector <CB> vec, CB &elem) {
-        std::vector <CB>::iterator iter;
-        std::vector <CB>::iterator iter_end = vec.end();
-        
-        for (iter = vec.begin (); iter != iter_end; iter++) {
-            if (*iter == elem) {
+    inline static bool vec_contains(const std::vector <std::unique_ptr<CB> > &vec, const CB &elem) {
+        for (auto & other : vec) {
+            if (*other == elem) {
                 return true;
             }
         }
         return false;
     }
+    /**
+     * Tests whether the transition contains the given cb.
+     */
+    inline bool IsNew(const CB &c) const {
+        return !vec_contains(intra_cb, c)  && !vec_contains(inter_cb, c);
+    }
 
-    inline bool Contains(CB &c) {
-        return vec_contains(_intra_cb, c) || vec_contains(_inter_cb, c);
-}
-    
-};
-
-class Transition {
-public:
-    Transition();
-    Transition(const Transition &t_, bool ref_t=false);
-    Transition(Envelope *e, bool ref_e=false);
-    
-    Transition &operator=(const Transition &t_);
-
-    void ref();
-    void unref();
-    void copy();
-    Envelope *GetEnvelope() const;
-    bool AddIntraCB(CB &c);
-    void AddInterCB(CB &c);
-    void moveCurrMatching();
-
-    void set_curr_matching(const CB &c);
-    CB &get_curr_matching() const;
-    std::vector<int> &get_ancestors() const;
-    std::vector<int> &mod_ancestors();
-#ifdef FIB
-    std::vector <CB> &get_inter_cb();
-    std::vector <CB> &mod_inter_cb();
-    std::vector <CB> &get_intra_cb();
-    std::vector <CB> &get_cond_intra_cb();
-
-    void AddCondIntraCB(CB &c);
-#endif
-
-    Transition_internal *t;
 };
   
 #endif
