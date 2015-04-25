f4abc89f99dc44928f6c96fb171ac7dda09eb1b2
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-04-25 17:35:54 -0500

    Compile transition list.

---
+++ src/TestTransitionList.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN 
+#include <catch.hpp>
+#include "TransitionList.hpp"
+
+TEST_CASE("Testing CB", "[wat]") {
+    // only smaller if the first component (the pid) is smaller
+    REQUIRE(CB(0, 100) < CB(1, 0));
+    // if pid is the same, then compare 
+    REQUIRE(CB(1, 0) < CB(1, 100));
+    // equality is defined
+    REQUIRE(CB(100, 100) == CB(100, 100));
+}
+
+
