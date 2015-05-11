5c269dc093e5466de7edae419b5deeb073362c07
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-05-11 12:39:36 -0500

    Adding more test cases.

--- src/TestExamples.cpp
+++ src/TestExamples.cpp
@@ -4,7 +4,7 @@
 #include "Generator.hpp"
 
 // DOI: 10.1007/978-3-642-11261-4_12
-TEST_CASE("ISP Tool Update: Scalable MPI Verification example-1") {
+TEST_CASE("ISP Tool Update: Scalable MPI Verification example-1.1") {
     /*
      * P0: Isend(to P1, &h0) ; Barrier; Wait(h0);
      * P1: Irecv(*, &h1)     ; Barrier; Wait(h1);
@@ -39,24 +39,6 @@ TEST_CASE("ISP Tool Update: Scalable MPI Verification example-1") {
     //trace.insert(c8);
     //Call c9(P2, 2, Envelope::Wait(1));
     //trace.insert(c9);
-    /*
-    // All calls must be enabled
-    auto procs = sort_by_procs(trace);
-
-    set<Call> proc0 = filter_enabled(procs[P0]);
-    REQUIRE(2 == proc0.size());
-    REQUIRE(proc0.find(c1) != proc0.end());
-    REQUIRE(proc0.find(c2) != proc0.end());
-
-    set<Call> proc1 = filter_enabled(procs[P1]);
-    REQUIRE(2 == proc1.size());
-    REQUIRE(proc1.find(c4) != proc1.end());
-    REQUIRE(proc1.find(c5) != proc1.end());
-
-    set<Call> proc2 = filter_enabled(procs[P2]);
-    REQUIRE(1 == proc2.size());
-    REQUIRE(proc2.find(c7) != proc2.end());
-    */
 
     auto ms = get_match_sets(trace);
     // the program is *deterministic*, so only one trace is allowed
@@ -69,6 +51,56 @@ TEST_CASE("ISP Tool Update: Scalable MPI Verification example-1") {
     REQUIRE(inter[2] == c7); // P2:Barrier
 }
 
+// DOI: 10.1007/978-3-642-11261-4_12
+TEST_CASE("ISP Tool Update: Scalable MPI Verification example-1.2") {
+    /*
+     * P0: Isend(to P1, &h0) ; Wait(h0);
+     * P1: Irecv(*, &h1)     ; Wait(h1);
+     * P2: Isend(to P1, &h2); Wait(h2);
+     */
+    const int P0 = 0, P1 = 1, P2 = 2;
+    // GET THE SECOND PHASE ONCE ALL PROCESSES ARE BLOCKED
+    set<Call> trace;
+    // P0:
+    Call c1(P0, 0, Envelope::ISend(P1));
+    trace.insert(c1);
+    //Call c2(P0, 1, Envelope::Barrier());
+    //trace.insert(c2);
+    Call c3(P0, 2, Envelope::Wait(0));
+    trace.insert(c3);
+
+    // P1:
+    Call c4(P1, 0, Envelope::IRecv(WILDCARD));
+    trace.insert(c4);
+    //Call c5(P1, 1, Envelope::Barrier());
+    //trace.insert(c5);
+    Call c6(P1, 2, Envelope::Wait(0));
+    trace.insert(c6);
+
+    // P2:
+    //Call c7(P2, 0, Envelope::Barrier());
+    //trace.insert(c7);
+    Call c8(P2, 1, Envelope::ISend(P1));
+    trace.insert(c8);
+    Call c9(P2, 2, Envelope::Wait(1));
+    trace.insert(c9);
+
+    auto ms = get_match_sets(trace);
+    // the program is *deterministic*, so only one trace is allowed
+    REQUIRE(2 == ms.size());
+    auto inter = ms[0].toVector();
+    REQUIRE(inter.size() == 2);
+    // notice how the IReceive any gets rewritten to receive from P0
+    REQUIRE(inter[0] == Call(P1, 0, Envelope::IRecv(P0))); // P1:IReceive
+    REQUIRE(inter[1] == c1); // P0:ISend(P1)
+
+    // Next interleaving
+    inter = ms[1].toVector();
+    REQUIRE(inter.size() == 2);
+    // notice how the IReceive any gets rewritten to receive from P2
+    REQUIRE(inter[0] == Call(P2, 0, Envelope::IRecv(P2))); // P1:IReceive
+    REQUIRE(inter[1] == c8); // P2:ISend(P1)
+}
 TEST_CASE("recev-any-1") {
     const int P0 = 0, P1 = 1, P2 = 2;
 
